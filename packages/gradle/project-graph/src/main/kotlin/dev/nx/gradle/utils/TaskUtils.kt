fe0d975ff479c52510aa1b6588342defa8d4aedb
Emily Xiong <xiongemi@gmail.com>
2025-05-20 15:28:30 -0400

    fix(gradle): change run and bootRun to cacheable false

--- packages/gradle/project-graph/src/main/kotlin/dev/nx/gradle/utils/TaskUtils.kt
+++ packages/gradle/project-graph/src/main/kotlin/dev/nx/gradle/utils/TaskUtils.kt
@@ -25,7 +25,7 @@ fun processTask(
   val logger = task.logger
   logger.info("NxProjectReportTask: process $task for $projectRoot")
   val target = mutableMapOf<String, Any?>()
-  target["cache"] = true // set cache to be always true
+  target["cache"] = isCacheable(task) // set cache to be always true
 
   val continuous = isContinuous(task)
   if (continuous) {
@@ -309,4 +309,8 @@ fun replaceRootInPath(p: String, projectRoot: String, workspaceRoot: String): St
 
 fun isContinuous(task: Task): Boolean {
   return task.name == "bootRun"
+}
+
+fun isCacheable(task: Task): Boolean {
+  return task.name != "bootRun" && task.name != "run"
 }
